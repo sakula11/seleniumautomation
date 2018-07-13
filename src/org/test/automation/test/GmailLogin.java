7619a28f6b4140636dc5e352441978106cdfbd12
Praveen28487 <40788502+Praveen28487@users.noreply.github.com>
2018-07-13 08:19:51 +0530

    Updated the scripts for Gmail
    Updated the scripts for Gmail

--- src/org/test/automation/test/GmailLogin.java
+++ src/org/test/automation/test/GmailLogin.java
@@ -1,6 +1,8 @@
 package org.test.automation.test;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.test.automation.base.BrowserManager;
 import org.testng.annotations.Test;
 
@@ -12,7 +14,27 @@ public class GmailLogin extends BrowserManager{
 	{
 		_Driver.get("https://mail.google.com");
 		
+		WebDriverWait wait = new WebDriverWait(_Driver, 30);
+		wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.linkText("Sign In")));
+		
+		
+		//xpath
+		
+//		_Driver.findElement(By.xpath("//a[text()='Sign In']")).click();
+		
 		_Driver.findElement(By.linkText("Sign In")).click();
+		
+		_Driver.findElement(By.id("identifierId")).sendKeys("testautomation");
+		
+		_Driver.findElement(By.xpath("//span[text()='Next']")).click();		
+		
+		_Driver.findElement(By.name("password")).sendKeys("tested");
+		
+		_Driver.findElement(By.xpath("//span[text()='Next']")).click();
+		
+		
+		
+		
 		
 
 
